da7a9004ccfcd969db40b74523cacc4b1b9544d1
Thalia <liaaglez@gmail.com>
2023-07-08 17:47:42 -0400

    nuevo

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter,
         Route} from 'react-router-dom'
 import { Menu } from './components/Menu'
 import {Login} from './components/Login'
+import { Navbar } from './components/Navbar'
+import { NotFound } from './components/NotFound'
+import {ProtectedRoute} from './components/ProtectedRoute'
 
 
 function AppRouter() {
@@ -12,8 +15,15 @@ function AppRouter() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route index element={<Login />} />
           <Route path='/menu' element={<Menu />}  />
+          <Route path='/inicio' element={
+            <ProtectedRoute >
+              <Navbar />
+            </ProtectedRoute>
+           } 
+            />
+          <Route path='*' element={<NotFound />}  />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,34 @@
-import React from "react"
+import React, {useContext, useState} from "react"
+import {useNavigate} from 'react-router-dom'
 import image from "../assets/logo.svg";
+import {AppContext} from '../context/AppContext'
 
 export const Login = () => {
+
+    const [users, setUsers] = useState([])
+
+    const handleChange = (e) =>{
+        setUsers({... users,[e.target.name]: e.target.value}) 
+    
+    }
+
+    let navigate = useNavigate();
+    
+    const {user}= useContext(AppContext)
+
+    const handleLogin = () =>{
+        if(user.find(u => u.username === users.username && u.password === users.password )){
+        localStorage.setItem('user',JSON.stringify(users))   
+            navigate('/inicio');
+        }else{
+            alert('incorrecto')
+        }
+    }
+
+    
+    
+   
+
     return (
         <main className="w-full h-screen flex flex-col items-center justify-center px-4">
             <div className="max-w-sm w-full text-gray-600">
@@ -18,11 +45,13 @@ export const Login = () => {
                 >
                     <div>
                         <label className="font-medium ">
-                            Email
+                            User
                         </label>
                         <input
-                            type="email"
+                            type="text"
                             required
+                            name='username'
+                            onChange={handleChange}
                             className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                         />
                     </div>
@@ -33,11 +62,14 @@ export const Login = () => {
                         <input
                             type="password"
                             required
+                            name='password'
+                            onChange={handleChange}
                             className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                         />
                     </div>
                     <button
                         className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                        onClick={handleLogin}
                     >
                         Entrar
                     </button>
